274af282455ec951a9f03226914d9e54ec2e2105
gerson <gersoncardena69@gmail.com>
2026-07-02 19:25:22 -0400

    chore: support API host dart define

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,5 +1,8 @@
 class ApiConfig {
-  static const String host = 'http://192.168.11.215:8000';
+  static const String host = String.fromEnvironment(
+    'API_HOST',
+    defaultValue: 'http://192.168.11.215:8000',
+  );
 
   static const String apiBaseUrl = '$host/api';
 }
